3bffd349f1ec3d70088f76ee46c94972cbe1ec47
abdulloyev <abdulloyev.uz@gmail.com>
2025-07-02 14:54:07 +0500

    add - document and window

---
+++ document and window parametrs/js/script.js
@@ -0,0 +1,24 @@
+const box = document.querySelector(".box"),
+  btn = document.querySelector("button");
+
+// btn.addEventListener("click", () => {
+//   console.log(box.scrollTop);
+// });
+
+// console.log(box.getBoundingClientRect());
+
+// console.log(window.getComputedStyle(box));
+
+// TODO: box emelentning o'lchamini olish
+// const width = box.clientWidth;
+// const height = box.clientHeight;
+
+// TODO: box elementning styl o'lchamini olish
+// const width = box.offsetWidth;
+// const height = box.offsetHeight;
+
+// TODO: box elementning scroll o'lchamini olish
+// const width = box.scrollWidth;
+// const height = box.scrollHeight;
+
+// console.log(width, height);
